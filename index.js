60c4ef37a8d6731b10ed4d14a55aece793a884ca
Aman7211 <amanrathore9811@gmail.com>
2023-08-24 17:29:34 +0530

    sixth

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 6000;
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://resumeprofile.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers", 
     "Origin, X-Requested-With, Content-Type, Accept"
